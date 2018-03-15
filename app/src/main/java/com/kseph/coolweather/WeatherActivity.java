954d8e4859547b91d8de19e50e4ca1a87eafeca9
kseph <912699357@qq.com>
2018-03-15 12:25:53 +0800

    增加后台自动更新天气功能.

--- app/src/main/java/com/kseph/coolweather/WeatherActivity.java
+++ app/src/main/java/com/kseph/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.kseph.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.kseph.coolweather.gson.Forecast;
 import com.kseph.coolweather.gson.Weather;
+import com.kseph.coolweather.service.AutoUpdateService;
 import com.kseph.coolweather.util.HttpUtil;
 import com.kseph.coolweather.util.Utility;
 
@@ -188,6 +190,9 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
     /**
